d8c4675d682979778fac7059ffbc6301b0b680b2
isabeladutra <dutraisabela88@gmail.com>
2020-04-23 22:15:25 -0300

    adicionando bean e fechando metodos de comunicacao com o bd

--- Sistema_farmacia/src/br/com/Sistema_farmacia/DAO/FornecedoresDAO.java
+++ Sistema_farmacia/src/br/com/Sistema_farmacia/DAO/FornecedoresDAO.java
@@ -75,12 +75,39 @@ public class FornecedoresDAO {
 		return retorno;
 	}
 
+	public ArrayList<Fornecedores>buscarPorDescricao(Fornecedores f) throws SQLException{
+		StringBuilder sql = new StringBuilder();
+		sql.append("SELECT codigo, descricao ");
+		sql.append("FROM fornecedores ");
+		sql.append("WHERE descricao LIKE ? ");
+		sql.append("ORDER BY descricao ASC ");
+		
+		Connection conexao = ConexaoFactory.conectar();
+
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setString(1, "%" + f.getDescricao() + "%");
+		ResultSet resultado = comando.executeQuery();
+		ArrayList<Fornecedores> lista = new ArrayList<Fornecedores>();
+		
+		while(resultado.next()) {
+			Fornecedores item = new Fornecedores();
+			item.setCodigo(resultado.getLong("codigo"));
+			item.setDescricao(resultado.getString("descricao"));
+			
+			lista.add(item);
+		}
+		
+		return lista;
+		
+		
+		
+	}
 	
 	public ArrayList<Fornecedores> Listar() throws SQLException{
 		StringBuilder sql = new StringBuilder();
 		sql.append("SELECT codigo, descricao ");
 		sql.append("FROM fornecedores ");
-		sql.append("ORDER BY descricao aSC ");
+		sql.append("ORDER BY descricao ASC ");
 
 		Connection conexao = ConexaoFactory.conectar();
 
@@ -93,47 +120,98 @@ public class FornecedoresDAO {
 		while(resultado.next()) {
 			Fornecedores f = new Fornecedores();
 			f.setCodigo(resultado.getLong("codigo"));
+			f.setDescricao(resultado.getString("descricao"));
 			
+			lista.add(f);
 		}
+		
+		return lista;
 		
 	}
 	
 	public static void main(String[] args) {
-		/*
-		 * Fornecedores f1 = new Fornecedores(); f1.setDescricao("carlos");
-		 * 
-		 * FornecedoresDAO fdao= new FornecedoresDAO(); try { fdao.salvar(f1);
-		 * System.out.println("Salvo com sucesso!"); } catch (SQLException e) { // TODO
-		 * Auto-generated catch block System.out.println("Erro ao salvar!");
-		 * e.printStackTrace(); }
+		
+		/*  Fornecedores f1 = new Fornecedores(); 
+		  f1.setDescricao("HUGO VASCONCELOS");
+		 // Fornecedores f2 = new Fornecedores(); 
+		  //f2.setDescricao("Descricao 2");
+		  FornecedoresDAO fdao= new FornecedoresDAO(); 
+		  try {
+			  fdao.salvar(f1);
+			 // fdao.salvar(f2);
+		  System.out.println("Salvo com sucesso!"); 
+		  } catch (SQLException e) {
+			  System.out.println("Erro ao salvar!");
+		  e.printStackTrace(); }
 		 */
-		/*
-		 * Fornecedores f1 = new Fornecedores(); f1.setCodigo(1);
-		 * 
-		 * FornecedoresDAO fdao= new FornecedoresDAO(); try { fdao.excluir(f1);
-		 * System.out.println("Deletado com sucesso!"); } catch (SQLException e) { //
-		 * TODO Auto-generated catch block System.out.println("Erro ao deletar!");
-		 * e.printStackTrace(); }
+		
+		 /* Fornecedores f1 = new Fornecedores(); 
+		  f1.setCodigo(2);
+		  
+		  FornecedoresDAO fdao= new FornecedoresDAO();
+		  try {
+			  fdao.excluir(f1);
+		  System.out.println("Deletado com sucesso!"); 
+		  } catch (SQLException e) { 
+		   System.out.println("Erro ao deletar!");
+		  e.printStackTrace(); }
 		 */
 
 		/*
-		 * Fornecedores f1 = new Fornecedores(); f1.setCodigo(2);
-		 * f1.setDescricao("Isabela");
-		 * 
-		 * FornecedoresDAO fdao= new FornecedoresDAO(); try { fdao.editar(f1);
-		 * System.out.println("Editado com sucesso!"); } catch (SQLException e) { //
-		 * TODO Auto-generated catch block System.out.println("Erro ao editar!");
-		 * e.printStackTrace(); }
+		  Fornecedores f1 = new Fornecedores();
+		  f1.setCodigo(3);
+		  f1.setDescricao("Isabela");
+		  
+		  FornecedoresDAO fdao= new FornecedoresDAO();
+		  try {
+			  fdao.editar(f1);
+		  System.out.println("Editado com sucesso!"); 
+		  } catch (SQLException e) { 
+		  System.out.println("Erro ao editar!");
+		  e.printStackTrace(); }
 		 */
 
-		Fornecedores f1 = new Fornecedores();
-		f1.setCodigo(1);
+		/*Fornecedores f1 = new Fornecedores();
+		f1.setCodigo(3);
+		
+		//Fornecedores f2 = new Fornecedores();
+		//f2.setCodigo(5);
 
 		FornecedoresDAO fdao = new FornecedoresDAO();
 		try {
-			fdao.buscaPorCodigo(f1);
-			System.out.println("Resultado: " + f1);
+			 Fornecedores f3 = fdao.buscaPorCodigo(f1);
+			//Fornecedores f4 = fdao.buscaPorCodigo(f2);
+			System.out.println("Resultado: " + f3);
+			//System.out.println("Resultado: " + f4);
 		} catch (SQLException e) {
+			
+			System.out.println("Erro ao salvar!");
+			e.printStackTrace();
+		}*/
+		
+		/*FornecedoresDAO fdao = new FornecedoresDAO();
+		try {
+			ArrayList<Fornecedores>Lista = fdao.Listar();
+			
+			for (Fornecedores f : Lista) {
+			System.out.println("Fornecedores" + f);
+			}
+			} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Erro ao salvar!");
+			e.printStackTrace();
+		}*/
+		
+		Fornecedores f1 = new Fornecedores();
+		f1.setDescricao("Is");
+		FornecedoresDAO fdao = new FornecedoresDAO();
+		try {
+			ArrayList<Fornecedores>Lista = fdao.buscarPorDescricao(f1);
+			
+			for (Fornecedores f : Lista) {
+			System.out.println("Fornecedores" + f);
+			}
+			} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			System.out.println("Erro ao salvar!");
 			e.printStackTrace();
